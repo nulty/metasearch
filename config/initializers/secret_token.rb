f8b183a2a42b930bb3a2991dd8d3d0f5ff08dc31
Iain McNulty <iainmcnulty@gmail.com>
2012-07-03 17:20:23 +0100

    Fresh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metasearch::Application.config.secret_token = 'dac3cfb9e1399d489235f47f000e73480f7fa06cca8d29c77a41dd73fe1a001787971ca186868f8c052eba1b093e53c78ca7e6744e83f416ef43cfda960b5b1f'
